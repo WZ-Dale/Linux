b8c9b543d005a89a4c096c67861d2c2732332e00
wangze <w18829898295@163.com>
2019-04-28 21:30:47 +0800

    子进程创建

---
+++ 20190324/vfork.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+  pid_t pid = vfork();
+  if(pid == 0){
+    printf("---child!! pid:[%d]\n",getpid());
+    exit(0);
+  }
+  else{
+    printf("parent!!--- pid:[%d]\n",getpid());
+  }
+  while(1){
+    printf("---pid:[%d]---\n",getpid());
+    sleep(1);
+  }
+  return 0;
+}
