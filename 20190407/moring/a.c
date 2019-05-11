76e139ed32e114f612847762b79f87cadc947646
wangze <w18829898295@163.com>
2019-05-11 17:42:43 +0800

    库的创建

---
+++ 20190407/moring/a.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "b.h"
+
+int main(){
+  printf("----a.c----");
+  print("----b.c----");
+  return 0;
+}
---
+++ 20190407/moring/b.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int print(){
+  printf("----b.c----");
+  return 0;
+}
